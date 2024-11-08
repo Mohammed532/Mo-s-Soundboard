d07a87f39f5a36ed04cbe5303e00c2df5ee43e1e
Mohammed532 <m.m.akinbayo@gmail.com>
2024-11-07 20:11:43 -0500

    Finished initial setup with Nextjs and Firebase

---
+++ devnote.md
@@ -0,0 +1,9 @@
+# Dev Notes
+
+## Todo
+
+### 1.0 Release
+
+- [ ] Landing page
+- [ ] Soundboard page
+- [ ] Audio files (local storage)
---
+++ src/_firebase/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAUhY32d8_Yxe-_E-c3jRPsqXJxCzxmCtA",
+  authDomain: "mo-s-soundboard.firebaseapp.com",
+  projectId: "mo-s-soundboard",
+  storageBucket: "mo-s-soundboard.firebasestorage.app",
+  messagingSenderId: "348568864637",
+  appId: "1:348568864637:web:b7bf61d24149c1136cee14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
