ea652b2be9368cafe8a55ac7b618c9f7d5adb169
Mohammed532 <m.m.akinbayo@gmail.com>
2024-11-07 20:54:33 -0500

    fixed error

--- devnote.md
+++ devnote.md
@@ -7,3 +7,5 @@
 - [ ] Landing page
 - [ ] Soundboard page
 - [ ] Audio files (local storage)
+- [ ] Setup Firebase Hosting
+- [ ] Setup CI/CD (for possible further dev, prolly won't happen)
--- src/_firebase/firebaseConfig.ts
+++ src/_firebase/firebaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUhY32d8_Yxe-_E-c3jRPsqXJxCzxmCtA",
-  authDomain: "mo-s-soundboard.firebaseapp.com",
-  projectId: "mo-s-soundboard",
-  storageBucket: "mo-s-soundboard.firebasestorage.app",
-  messagingSenderId: "348568864637",
-  appId: "1:348568864637:web:b7bf61d24149c1136cee14"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
